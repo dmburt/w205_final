a2ce7f44e91ae365ba8fd09563d63446681f32fd
dmburt <dmburt@berkeley.edu>
2016-04-23 14:44:14 -0400

    Added conversion of national summary data to csv

--- start/02_prep_data.sh
+++ start/02_prep_data.sh
@@ -6,6 +6,10 @@ mkdir /data/final/data/import
 head -20000 /data/final/data/npidata_20050523-20160410.csv > /data/final/data/import/provider-mini.csv
 head -20000 /data/final/data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > /data/final/data/import/prescription-mini.tab
 
-python apd_etl.py
+wait && python convert_ntl_rx.py
+
+cp /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv /data/final/data/import/PartD_Prescriber_PUF_Drug_Ntl_13.csv
+
+wait && python apd_etl.py
 
 
