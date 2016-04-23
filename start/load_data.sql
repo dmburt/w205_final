c9fa1bc42033d3e6a38e51ba462dc8522a90ca93
junluo-code <junluo168@outlook.com>
2016-04-23 18:43:53 -0400

    init prescription table calculated fields with 0.0

--- start/load_data.sql
+++ start/load_data.sql
@@ -5,22 +5,45 @@ CREATE TABLE providers (
 );
 
 CREATE TABLE prescriptions (
-    npi                 varchar(10),
-    lastname            varchar(100),
-    firstname           varchar(100),
-    city                varchar(100),
-    state               varchar(2),
-    specialty           varchar(100),
+    npi   		varchar(10),
+    lastname    	varchar(100),    
+    firstname       	varchar(100),
+    city            	varchar(100),
+    state    		varchar(2),    
+    specialty       	varchar(100),
     description_flag    varchar(1),
-    drugname            varchar(100),
+    drugname    	varchar(100),    
     genericname         varchar(100),
-    benecount           int,
-    claimcount          int,
-    daysupply           int,
-    drugcost            real
+    benecount 		int,
+    claimcount 		int,
+    daysupply 		int,
+    drugcost 		real,
+    claimsperbene       real DEFAULT 0.0,
+    costperbene         real DEFAULT 0.0,
+    daysperclaim        real DEFAULT 0.0,
+    daysperclaimperbene real DEFAULT 0.0
 );
+
+
 
 COPY providers FROM '/data/final/data/import/provider-import.csv' DELIMITER ',' CSV HEADER;
 
-COPY prescriptions FROM '/data/final/data/import/prescription-import.csv' DELIMITER ',' CSV HEADER;
+COPY prescriptions
+(
+    npi,
+    lastname ,   
+    firstname,
+    city,
+    state, 
+    specialty,
+    description_flag,
+    drugname,
+    genericname,
+    benecount,
+    claimcount,
+    daysupply,
+    drugcost
+)
+FROM '/data/final/data/prescription-import.csv' DELIMITER ',' CSV HEADER;
+
 
