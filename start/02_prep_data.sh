ebdb0ebeeb3e98c6c451551730b4ff9fcf9a4b19
dmburt <dmburt@berkeley.edu>
2016-04-23 17:02:57 -0400

    Added conversion of national summary data to csv
    Python Excel-to-CSV conversion module now fed file name as parameter.

--- start/02_prep_data.sh
+++ start/02_prep_data.sh
@@ -6,7 +6,7 @@ mkdir /data/final/data/import
 head -20000 /data/final/data/npidata_20050523-20160410.csv > /data/final/data/import/provider-mini.csv
 head -20000 /data/final/data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > /data/final/data/import/prescription-mini.tab
 
-wait && python convert_ntl_rx.py
+wait && python convert_ntl_rx.py /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv
 
 cp /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv /data/final/data/import/PartD_Prescriber_PUF_Drug_Ntl_13.csv
 
