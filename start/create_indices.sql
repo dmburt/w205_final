91ed034972038719d2624611b6e09d91fb034b30
dmburt <dmburt@berkeley.edu>
2016-04-24 18:01:45 -0400

    Removed specialty index
    Not needed at this step--moved to larger covering index in script 6.

--- start/create_indices.sql
+++ start/create_indices.sql
@@ -4,4 +4,3 @@ CREATE UNIQUE INDEX pk_providers ON providers (NPI);
 
 CREATE UNIQUE INDEX pk_prescriptions ON prescriptions (NPI, drugname, genericname);
 
-CREATE INDEX idx_specialty_drug ON prescriptions (specialty, drugname, genericname);
