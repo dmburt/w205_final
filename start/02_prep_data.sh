c36f3fe0e2f63ed346795b7f70722e90ed1e00fc
dmburt <dmburt@berkeley.edu>
2016-04-24 11:43:09 -0400

    Changed prescription national file name

--- start/02_prep_data.sh
+++ start/02_prep_data.sh
@@ -9,7 +9,7 @@ head -20000 /data/final/data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > /data/final/
 # National prescription data provided by CMS as an Excel file.
 # This conversion utility produces a CSV file from the Excel source.
 wait && python convert_ntl_rx.py /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv
-cp /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv /data/final/data/import/prescription-national.csv
+cp /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv /data/final/data/import/prescription-natl.csv
 
 # Provider file is encoded in UTF-8, but petl library expects ASCII.
 # This utility will do the conversion, creating a new copy with "_ascii" in the file name.
