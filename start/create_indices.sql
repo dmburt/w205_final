e85d48e0a0ed3afc4df34a4e0415dcd2acf0f7a1
dmburt <dmburt@berkeley.edu>
2016-04-24 17:41:48 -0400

    Created covering index for specialty rollup

--- start/create_indices.sql
+++ start/create_indices.sql
@@ -3,3 +3,5 @@ CREATE UNIQUE INDEX pk_prescriptions_natl ON prescriptions_natl (drugname, gener
 CREATE UNIQUE INDEX pk_providers ON providers (NPI);
 
 CREATE UNIQUE INDEX pk_prescriptions ON prescriptions (NPI, drugname, genericname);
+
+CREATE INDEX idx_specialty_drug ON prescriptions (specialty, drugname, genericname);
