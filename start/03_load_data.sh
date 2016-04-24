6384993d82321a207938e401d91a214670c6d70e
dmburt <dmburt@berkeley.edu>
2016-04-24 16:15:13 -0400

    Added index creation

--- start/03_load_data.sh
+++ start/03_load_data.sh
@@ -3,5 +3,6 @@
 psql -h localhost -U postgres -c "DROP DATABASE IF EXISTS apd;"
 psql -h localhost -U postgres -c "CREATE DATABASE apd;"
 psql -h localhost -U postgres -d apd -f load_data.sql
+wait && psql -h localhost -U postgres -d apd -f create_indices.sql
 
 
