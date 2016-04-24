4d16cb978d7d4aa22a48d5e50f62f00ba9763acb
dmburt <dmburt@berkeley.edu>
2016-04-24 11:36:11 -0400

    Added comments

--- start/02_prep_data.sh
+++ start/02_prep_data.sh
@@ -6,6 +6,8 @@ mkdir /data/final/data/import
 head -20000 /data/final/data/npidata_20050523-20160410.csv > /data/final/data/import/provider-mini.csv
 head -20000 /data/final/data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > /data/final/data/import/prescription-mini.tab
 
+# National prescription data provided by CMS as an Excel file.
+# This conversion utility produces a CSV file from the Excel source.
 wait && python convert_ntl_rx.py /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv
 cp /data/final/data/PartD_Prescriber_PUF_Drug_Ntl_13.csv /data/final/data/import/prescription-national.csv
 
